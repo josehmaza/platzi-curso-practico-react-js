5757c237acc958da5111fab4a318b09fa34bba42
josehmaza <josehmaza@gmail.com>
2020-02-23 15:14:14 -0500

    Estilos del Header

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import Header from '../components/Header'
+import '../assets/styles/App.scss';
+
 const App = () => (
     <div className="App">
         <Header></Header>
