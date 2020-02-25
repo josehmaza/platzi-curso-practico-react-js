b7ea7068956610cf92838ae2de7eac0683c1ec19
josehmaza <josehmaza@gmail.com>
2020-02-25 13:37:31 -0500

    Vista geeneral del player

--- src/components/CarouselItem.jsx
+++ src/components/CarouselItem.jsx
@@ -6,6 +6,7 @@ import playIcon from '../assets/static/play-icon.png'
 import plusIcon from '../assets/static/plus-icon.png'
 import removeIcon from '../assets/static/remove-icon.png'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 import { setFavourite, deleteFavourite } from '../actions'
 const CarousellItem = (props) => {
     const { id, cover, title, year, contentRating, duration, isList } = props;
@@ -24,12 +25,14 @@ const CarousellItem = (props) => {
                 <div>
                     {isList ?
                         <img className="carousel-item__details--img" src={removeIcon} alt="Remove Icon" onClick={() => handleDeleteFavourite(id)} />
-
                         :
                         <img className="carousel-item__details--img" src={plusIcon} alt="Plus Icon" onClick={handleSetFavourite} />
 
                     }
-                    <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
+                    <Link to={`/player/${id}`}>
+                        <img className="carousel-item__details--img" src={playIcon} alt="Play Icon" />
+
+                    </Link>
                 </div>
                 <p className="carousel-item__details--title">{title}</p>
                 <p className="carousel-item__details--subtitle">{`${year} ${contentRating} ${duration}`}</p>
---
+++ src/containers/Player.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Player = () => {
+    return (
+        <div className="Player">
+            <video controls autoPlay>
+                <source src="" type="video/mp4"/>
+            </video>
+            <div className="player-back">
+                <button type="button">Regresar</button>
+            </div>
+        </div>
+
+    )
+}
+
+export default Player;
